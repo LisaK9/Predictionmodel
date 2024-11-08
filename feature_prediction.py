e29d4df1c31f897282ea0074c8298da041b980f6
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2024-11-08 17:16:26 +0100

    Schätzung von calls und n_sick und erstellen der Features für Modelltraining - Dev

--- feature_prediction.py
+++ feature_prediction.py
@@ -24,21 +24,8 @@ data['month'] = data['month'].astype(int)
 data['holiday'] = data['holiday'].astype(int)
 data['season'] = data['season'].astype(int)
 data['day'] = data['day'].astype(int)
-
-# Lags hinzufügen
-for lag in range(1, 31):  # Lags für die letzten 7 Tage
-    data[f'lag_{lag}'] = data['calls'].shift(lag)
-
-for lag in range(1, 31):  # Lags für die letzten 7 Tage
-    data[f'lag_sby_need_{lag}'] = data['sby_need'].shift(lag)
-print(data)
-
-for lag in range(1, 31):  # Lags für die letzten 7 Tage
-    data[f'lag_sick_{lag}'] = data['n_sick'].shift(lag)
-print(data)
-
-# Fehlende Werte aufgrund der Lags entfernen
-data.dropna(inplace=True)
+data['day_of_week'] = data['day_of_week'].astype(int)
+data['month_day_interaction'] = (data['month'] - 1) * 7 + data['day_of_week']
 
 # Testzeitraum festlegen:
 
@@ -54,25 +41,21 @@ test_data = data[(data['date'] >= test_start_date) & (data['date'] <= test_end_d
 getestet. Die besten Ergebnisse konnte die hier vorhandene Kombination erzielen"""
 
 # Zeitliche Variablen und Lags als Zusatzregressoren hinzufügen
-train_prophet = train_data[['date', 'calls', 'month','holiday', 'season', 'lag_30','lag_sby_need_30', 'lag_sick_30', 'day']].rename(columns={'date': 'ds', 'calls': 'y'})
+train_prophet = train_data[['date', 'calls','holiday','month', 'season',  'month_day_interaction']].rename(columns={'date': 'ds', 'calls': 'y'})
 
 # Prophet initialisieren
 model = Prophet(changepoint_prior_scale=0.5, seasonality_prior_scale=9, interval_width=0.95, seasonality_mode='multiplicative')
-model.add_seasonality(name='monthly', period=30.5, fourier_order=20)
+model.add_seasonality(name='monthly', period=30.5, fourier_order=26)
 model.add_regressor('holiday')
 model.add_regressor('month')
 model.add_regressor('season')
-model.add_regressor('lag_30')
-model.add_regressor('lag_sby_need_30')
-model.add_regressor('lag_sick_30')
-model.add_regressor('day')
-
+model.add_regressor('month_day_interaction')
 # Modell trainieren
 model.fit(train_prophet)
 
 
 # Vorbereitung der Daten für die Prognose
-future_dates = test_data[['date', 'holiday', 'month','season', 'lag_30','lag_sby_need_30','lag_sick_30', 'day']].rename(columns={'date': 'ds'})
+future_dates = test_data[['date', 'holiday','month','season', 'month_day_interaction']].rename(columns={'date': 'ds'})
 
 # Prognosen generieren
 forecast = model.predict(future_dates)
@@ -103,7 +86,7 @@ plt.show()
 
 """Schätzung des Feature n_sick. Hier wurden ebenfalls mehrere Kombinationen getestet"""
 
-train_prophet2 = train_data[['date', 'n_sick', 'day_of_week', 'month','holiday', 'season', 'lag_30','lag_sby_need_30', 'lag_sick_30']].rename(columns={'date': 'ds', 'n_sick': 'y'})
+train_prophet2 = train_data[['date', 'n_sick', 'day_of_week', 'month','holiday', 'season']].rename(columns={'date': 'ds', 'n_sick': 'y'})
 
 # Prophet initialisieren
 model2 = Prophet(changepoint_prior_scale=0.35)
@@ -117,7 +100,7 @@ model2.add_regressor('season')
 model2.fit(train_prophet2)
 
 # Vorbereitung der Daten für die Prognose von n_sick
-future_dates2 = test_data[['date', 'day_of_week', 'holiday', 'month','season', 'lag_30','lag_sby_need_30','lag_sick_30']].rename(columns={'date': 'ds'})
+future_dates2 = test_data[['date', 'day_of_week', 'holiday', 'month','season']].rename(columns={'date': 'ds'})
 print('futuredates: ',future_dates2)
 # Prognosen generieren
 forecast2 = model2.predict(future_dates2)
