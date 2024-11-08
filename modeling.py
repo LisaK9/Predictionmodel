4cb45e75f7dfd12aa9e9c708660ea60401586fe4
lisa0908 <108758315+lisa0908@users.noreply.github.com>
2024-11-08 17:50:30 +0100

    Baseline, Feature-Selektion, Modell-Training und evaluation der ersten Ergebnisse, um ein Modell für die Vorhersage auszuwählen

---
+++ modeling.py
@@ -0,0 +1,261 @@
+import modeling_functions
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+
+#Daten laden
+data = modeling_functions.load_and_preprocess_data('sickness_modeling.csv')
+print(data)
+
+#Features und Zielvariable definieren
+X, y, features = modeling_functions.define_features_and_target(data)
+print("Features: ", features)
+#Train Test Split
+X_train, X_test, y_train, y_test = modeling_functions.split_data(X, y)
+
+#Speichern der Testwerte zum späteren Vergleich
+result_df=y_test.copy()
+result_df=pd.DataFrame(result_df)
+result_df=result_df.reset_index()
+
+#baseline Modell
+results={}
+predictions_dict = {}
+residuals_dict={}
+baseline = LinearRegression()
+mae, mse, rmse, r2, residuals, y_pred = modeling_functions.train_model_without_tuning(baseline, X_train,y_train,X_test,y_test)
+print("Baseline MSE: ", mse)
+
+results['baseline'] = {
+        'MAE': mae,
+        'MSE': mse,
+        'RMSE': rmse,
+        'R2': r2
+    }
+residuals_dict['baseline'] = residuals
+predictions_dict['baseline'] = y_pred
+
+# Modelle und Hyperparameter definieren
+models, param_grid = modeling_functions.define_models_and_params()
+
+#Feature Selectors
+feature_selectors = modeling_functions.define_feature_selectors()
+
+#Feature-Selektion, Modell-Training, Hyperparameter-Tuning
+best_estimators = {}
+best_scalers = {}
+best_selectors = {}
+best_params = {}
+model_best_combinations = {}
+for model_name, model in models.items():
+        print(f"\nTraining for model: {model_name}")
+        score = float('inf')
+        best_score = float('inf')
+        best_r2 = float('inf')
+        best_mae = float('inf')
+        best_rmse = float('inf')
+        best_fs_name = ""
+        best_n_features = 0
+        best_model = None
+        best_scaler = None
+        best_selected_features = []
+        best_predictions = None
+        best_residuals = None
+
+        for fs_name, selector in feature_selectors.items(): #Feature Selektion
+            print(f"  Using feature selection method: {fs_name}")
+
+            for n_features in range(1, len(features) + 1): #Jede Anzahl an Features durchlaufen
+                print(f"    Selecting {n_features} features with method {fs_name}...")
+                selected_feature_indices = modeling_functions.feature_selection(fs_name,selector,X_train,
+                                                                         y_train,n_features)
+                print("Selected Future Indices: ", selected_feature_indices)
+                selected_features = [features[i] for i in selected_feature_indices]
+                print(f"      Selected features ({len(selected_features)}): {selected_features}")
+
+                X_train_scaled, X_test_scaled, scaler = modeling_functions.scale_features(X_train, X_test, selected_features)
+                print(f"      Features scaled using StandardScaler.")
+
+                # Modelltraining ohne GridSearch
+                print(f"      Training model {model_name} with {fs_name} ({n_features} features) without tuning...")
+                mae, mse, rmse, r2, residuals, y_pred = modeling_functions.train_model_without_tuning(model,X_train_scaled,y_train,X_test_scaled,y_test)
+                print(
+                    f"        Evaluation without tuning completed. MAE: {mae:.4f}, MSE: {mse:.4f}, RMSE: {rmse:.4f}, R2: {r2:.4f}")
+                # Variablen zum Speichern der besten Ergebnisse initialisieren
+                current_mse = mse
+                current_model = model
+                current_params = None  #
+                if hasattr(model, 'get_params'):
+                    current_params = model.get_params()  # Standardparameter holen, falls verfügbar
+                current_mae = mae
+                current_rmse = rmse
+                current_r2 = r2
+                current_y_pred = y_pred
+                current_residuals = residuals
+
+                #Prüfen ob enthalten in param_grid, wenn ja, Hyperparameter-Tuning durchführen
+                if model_name in param_grid:
+                    best_estimator, best_params, mae_tuned, mse_tuned, rmse_tuned, r2_tuned, residuals_tuned, y_pred_tuned = modeling_functions.train_model_with_gridsearch(model,X_train_scaled,y_train,X_test_scaled,y_test,param_grid[model_name])
+                    print(f"        GridSearchCV completed for {model_name}. Best parameters: {best_params}")
+                    print(
+                        f"        Evaluation completed. MAE: {mae_tuned:.4f}, MSE: {mse_tuned:.4f}, RMSE: {rmse_tuned:.4f},"
+                        f" R2: {r2_tuned:.4f}")
+
+                    # Vergleich ohne/mit Tuning: Modell mit dem kleineren MSE behalten
+                    if mse_tuned < current_mse:
+                        current_mse = mse_tuned
+                        current_model = best_estimator
+                        current_params = best_params
+                        current_mae = mae_tuned
+                        current_rmse = rmse_tuned
+                        current_r2 = r2_tuned
+                        current_y_pred = y_pred_tuned
+                        current_residuals = residuals_tuned
+                        print(f"        GridSearch result chosen (better MSE).")
+                    else:
+                        print(f"        Standard model without tuning chosen (better MSE).")
+                else:
+                    print(f"        Model trained with standard hyperparameters.")
+
+                # beste Kombination für jedes Modell anhand MSE auswählen
+                if current_mse < best_score:
+
+                    print(f"        New best score found for model {model_name}: MSE {current_mse:.4f}")
+                    best_score = current_mse
+                    best_fs_name = fs_name
+                    best_n_features = n_features
+                    best_model = current_model
+                    best_scaler = scaler
+                    best_selected_features = selected_features
+                    best_predictions = y_pred
+                    best_params[f"{model_name} with {fs_name}"] = current_params
+                    best_mae = current_mae
+                    best_rmse = current_rmse
+                    best_r2 = current_r2
+                    best_residuals = current_residuals
+
+        # Modell Evaluation für jedes Modell speichern
+        key = f"{model_name} with {best_fs_name} ({best_n_features} features)"
+        results[key] = {
+            'MAE': best_mae,
+            'MSE': best_score,
+            'RMSE': best_rmse,
+            'R2': best_r2
+        }
+        best_estimators[key] = best_model
+        best_scalers[key] = best_scaler
+        best_selectors[key] = selector
+        predictions_dict[key] = best_predictions
+        residuals_dict[key] = best_residuals
+
+        # Beste Kombination für jedes Modell speichern
+        model_best_combinations[key] = {
+            'fs_name': best_fs_name,
+            'n_features': best_n_features,
+            'selected_features': best_selected_features,
+            'scaler': best_scaler,
+            'best_model': best_model
+        }
+
+# Ergebnisse der Evaluation der Modelle als Dataframe speichern und ausgeben
+results_evaluation = pd.DataFrame(results).T
+print("\nErgebnisse der Modelle:\n", results_evaluation)
+results_evaluation.to_csv('model_evaluation_results.csv', index=True)  # Speichern der Modellevaluation als CSV
+# Hinzufügen der Vorhersagen der Modelle zu result_df
+for model_name, predictions in predictions_dict.items():
+    result_df[model_name + '_pred'] = predictions
+print("\nDataFrame mit Vorhersagen:\n", result_df)
+
+# Learning Curves der besten Kombinationen als Visualisierung ausgeben
+learning_curves_data = {}
+for model_name, combo in model_best_combinations.items():
+    print(f"\nBeste Kombination für Modell {model_name}:")
+    print(f"  Feature-Selection Methode: {combo['fs_name']}, Anzahl Features: {combo['n_features']}, Ausgewählte Features: {combo['selected_features']}")
+    n_features = combo['n_features']
+    selected_features = combo['selected_features']
+    scaler = combo['scaler']
+    best_model = combo['best_model']
+    print("best_model: ", best_model)
+    fs_name = combo['fs_name']
+
+
+    #Trainings-/Validierungsfehler der trainierten Modelle
+    X_train_best = scaler.transform(X_train[selected_features])
+    y_train_best = y_train
+
+    train_sizes, train_scores, test_scores = learning_curve(best_model, X_train_best, y_train_best, cv=5,
+                                                            scoring='neg_mean_squared_error')
+    train_scores_mean = -np.mean(train_scores, axis=1)
+    test_scores_mean = -np.mean(test_scores, axis=1)
+
+    plt.figure()
+    plt.plot(train_sizes, train_scores_mean, label='Training error')
+    plt.plot(train_sizes, test_scores_mean, label='Validation error')
+    plt.title(f'Learning Curve ({model_name} with {fs_name} - {n_features} features)')
+    plt.xlabel('Training examples')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid()
+
+    # Lernkurven speichern
+    filename = f"learning_curve_{model_name}_{n_features}_features.png"
+    modeling_functions.save_plot(filename)
+    plt.show()
+
+    # Speichern der Learning Curve-Daten in CSV
+    learning_curves_data[f'{model_name}_train_sizes'] = train_sizes
+    learning_curves_data[f'{model_name}_train_scores'] = train_scores_mean
+    learning_curves_data[f'{model_name}_val_scores'] = test_scores_mean
+
+learning_curves_df = pd.DataFrame(learning_curves_data)
+learning_curves_df.to_csv('learning_curves_data.csv', index=False)  # Speichern der Learning Curve-Daten als CSV
+
+# Visualisierung der Vorhersagen im Vergleich zu den tatsächlichen Werten von jedem Modell
+plt.figure(figsize=(15, 10))
+for model_name in predictions_dict.keys():
+    plt.plot(result_df.index, result_df['sby_need'], label='Actual', color='blue')
+    plt.plot(result_df.index, result_df[model_name + '_pred'], label='Predicted', color='grey')
+    plt.title(f'Actual vs Predicted for {model_name}')
+    plt.xlabel('Test Data Index')
+    plt.ylabel('Anzahl benötigte Bereitschaftsfahrer (sby_need)')
+    plt.legend()
+    filename = f"predictions_vs_actual_{model_name}.png"
+    modeling_functions.save_plot(filename)
+    plt.show()
+
+#Visualisierung der Residuen von jedem Modell
+plt.figure(figsize=(15, 10))
+for model_name, residuals in residuals_dict.items():
+    plt.scatter(result_df.index, residuals, label='Residuals', alpha=0.5)
+    plt.axhline(0, color='black', linestyle='--', linewidth=1)
+    plt.title(f'Residuals Plot for {model_name}')
+    plt.xlabel('Test Data Index')
+    plt.ylabel('Residuals')
+    plt.legend()
+    plt.grid(True)
+    filename = f"residuals_{model_name}.png"
+    modeling_functions.save_plot(filename)
+    plt.show()
+
+#Visualisierung des MSE von jedem Modell
+mse_values = [results[key]['MSE'] for key in results]
+model_names = list(results.keys())
+model_names = [name.split("with")[0].strip() for name in model_names]
+
+plt.figure(figsize=(12, 6))
+bars = plt.bar(model_names, mse_values, color='skyblue')
+
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, yval, round(yval, 4), ha='center', va='bottom')
+
+plt.ylabel('Mean Squared Error (MSE)')
+plt.title('MSE of Each Best Model')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+modeling_functions.save_plot("MSE-Vergleich.png")
+plt.show()
+
+
---
+++ modeling_functions.py
@@ -0,0 +1,278 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.feature_selection import RFE, SelectKBest, f_regression
+from lightgbm import LGBMRegressor
+from catboost import CatBoostRegressor
+import xgboost as xgb
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR
+import pickle
+import matplotlib.pyplot as plt
+
+
+def load_and_preprocess_data(filepath):
+    """
+        Ladet die CSV-Datei mit den Daten für das Modelltraining, wandelt das Datum um und
+        setzt es als Index. Da der Bereitschaftsplan immer am 15. eines Monats zur Verfügung stehen muss,
+        werden nur Daten bis zum 14. eingeschlossen, mit den Daten bis zum Vortag trainiert werden soll.
+        Damit das Modell und die Vorhersagen präzise evaluiert werden kann, werden die vorhandenen Daten
+        des Folgemonats als Testdaten für das Modell, bzw. später als Input für die Vorhersage verwendet.
+        Args:
+        filepath (str): Der Pfad zur CSV-Datei.
+
+        Returns:
+        pd.DataFrame: DataFrame mit den Trainingsdaten gefiltert bis zum 14.04.2019.
+
+        Nach der Inbetriebnahme werden die Daten dann wie folgt geladen:
+
+        data = pd.read_csv(filepath)
+        data['date'] = pd.to_datetime(data['date'])
+        data.set_index('date', inplace=True)
+        max_date = data.index.max() # Max-Datum bestimmen
+        max_date_limit = max_date.replace(day=14)  # Setze den Tag auf den 14. Dies dient ausschließlich zur Sicherheit, normalerweise sollte das Max-Datum ausreichen
+
+        filtered_data = data[data.index <= max_date_limit] # Daten bis zum 14. des Max-Datums filtern
+
+        return filtered_data
+
+        """
+    data = pd.read_csv(filepath)
+    data['date'] = pd.to_datetime(data['date'])
+    #data['day'] = data['date'].dt.day
+    data.set_index('date', inplace=True)
+    return data[:'2019-04-14']
+
+
+# Features und Zielvariable festlegen
+def define_features_and_target(data):
+    """
+    Hier werden die Features und die Zielvariable, die für das Modelltraining verwendet werden,
+       festgelegt.
+    Args:
+        data (pd.DataFrame): DataFrame mit den Trainingsdaten.
+
+    Returns:
+        data[features] (pd.DataFrame), enthält die Daten der Features
+        data[target] (pd.Series), enthält die Daten der Zielvariable
+        features (List), enthält eine Liste der verwendeten Features.
+       """
+    features = ['n_duty', 'month', 'day_of_week', 'season', 'week', 'holiday', 'year', 'calls', 'n_sick'
+                ]
+    target = 'sby_need'
+    return data[features], data[target], features
+
+# Train-Test-Split durchführen
+def split_data(X, y):
+    """
+       Führt die Aufteilung der Daten in Trainings- und Testdaten durch.
+
+    Args:
+        X (pd.DataFrame): Daten der Features.
+        y (pd.Series): Daten der Zielvariable.
+
+    Returns:
+        Trainings- und Testdaten (X_train, X_test, y_train, y_test).
+       """
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def define_feature_selectors():
+    """
+       Hier werden die Feature-Selektionmethoden definiert, die verwendet werden sollen.
+       Returns:
+        Dictionary mit den Namen der Selektoren und den entsprechenden Instanzen
+       """
+    feature_selectors = {
+        'RFE': RFE(estimator=Ridge(alpha=1.0), n_features_to_select=None),  # Keine feste Anzahl von Features
+        'Random Forest': RandomForestRegressor(random_state=42)
+    }
+    return feature_selectors
+
+
+
+def define_models_and_params():
+    """
+       Definiert die Modelle und Hyperparameter-Raster für das Modelltraining.
+
+    Returns:
+        models: Dictionary der Modelle und dem
+        param_grid: Dictionary der Hyperparameter-Raster
+       """
+    models = {
+        #"Linear Regression": LinearRegression(),
+        "Random Forest Regressor": RandomForestRegressor(random_state=42),
+        "Gradient Boosting Regressor": GradientBoostingRegressor(random_state=42),
+        "XGBoost Regressor": xgb.XGBRegressor(objective='reg:squarederror', random_state=42),
+        "LightGBM Regressor": LGBMRegressor(random_state=42),
+        "CatBoost Regressor": CatBoostRegressor(logging_level='Silent', random_state=42)
+    }
+
+    param_grid = {
+        "Random Forest Regressor": {
+            'n_estimators': [50, 100, 200],
+            'max_depth': [3, 4, 5]
+        },
+        "Gradient Boosting Regressor": {
+            'n_estimators': [50, 100, 200],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'max_depth': [3, 4, 5]
+        },
+        "XGBoost Regressor": {
+            'n_estimators': [50, 100, 200],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'max_depth': [3, 4, 5]
+        },
+        "LightGBM Regressor": {
+            'n_estimators': [50, 100, 200],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'num_leaves': [31, 50, 100]
+        },
+        "CatBoost Regressor": {
+            'iterations': [100, 200, 300],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'depth': [3, 4, 5]
+        }
+    }
+    return models, param_grid
+
+
+
+def feature_selection(fs_name, selector, X_train, y_train, n_features):
+    """
+    Führt die Feature-Auswahl basierend auf der Feature-Selektionstechnik durch.
+    Args:
+        fs_name (str): Der Name der Feature-Selektionstechnik.
+        selector (object): Der Selektor, der verwendet wird.
+        X_train (pd.DataFrame): Die Trainingsdaten.
+        y_train (pd.Series): Die Zielvariable.
+        n_features (int): Die Anzahl der auszuwählenden Features.
+
+    Returns:
+        np.ndarray: Indizes der ausgewählten Features.
+    """
+    if fs_name == 'RFE':
+        selector.n_features_to_select = n_features
+        selector.fit(X_train, y_train)
+        selected_feature_indices = selector.get_support(indices=True)
+    elif fs_name == 'Random Forest':
+        selector.fit(X_train, y_train)
+        importances = selector.feature_importances_
+        indices = np.argsort(importances)[::-1]
+        selected_feature_indices = indices[:n_features]
+    else:
+        print(f"  Warning: Unknown feature selector {fs_name}, using default method.")
+        selector.fit(X_train, y_train)
+        selected_feature_indices = selector.get_support(indices=True)
+
+    return selected_feature_indices
+
+
+def scale_features(X_train, X_test, selected_features):
+    """
+    Skaliert die ausgewählten Features des Trainings- und Test-Sets.
+    Args:
+        X_train (pd.DataFrame): Die Trainingsdaten.
+        X_test (pd.DataFrame): Die Testdaten.
+        selected_features (list): Liste der auszuwählenden Features.
+
+    Returns:
+        X_train_scaled: skalierte Trainingsdaten
+        X_test_scaled: skalierte Testdaten
+        scaler: verwendeter Scaler
+    """
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train[selected_features])
+    X_test_scaled = scaler.transform(X_test[selected_features])
+    return X_train_scaled, X_test_scaled, scaler
+
+
+def train_model_without_tuning(model, X_train_scaled, y_train, X_test_scaled, y_test):
+    """
+    Trainiert das Modell ohne Hyperparameter-Tuning und berechnet die Werte zur Evaluierung.
+
+    Args:
+        model (object): Das zu trainierende Modell.
+        X_train_scaled (np.ndarray): Die skalierten Trainingsdaten.
+        y_train (pd.Series): Die Zielvariable für die Trainingsdaten.
+        X_test_scaled (np.ndarray): Die skalierten Testdaten.
+        y_test (pd.Series): Die Zielvariable für die Testdaten.
+
+    Returns:
+        MAE, MSE, RMSE, R², Residuen und Vorhersagen
+    """
+    model.fit(X_train_scaled, y_train)
+    y_pred = model.predict(X_test_scaled)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+    residuals = y_test - y_pred
+    return mae, mse, rmse, r2, residuals, y_pred
+
+
+def train_model_with_gridsearch(model, X_train_scaled, y_train, X_test_scaled, y_test, param_grid):
+    """
+    Trainiert das Modell mit GridSearch und berechnet die Werte für die Evaluierung.
+
+    Args:
+        model (object): Das zu trainierende Modell.
+        X_train_scaled (np.ndarray): Die skalierten Trainingsdaten.
+        y_train (pd.Series): Die Zielvariable für die Trainingsdaten.
+        X_test_scaled (np.ndarray): Die skalierten Testdaten.
+        y_test (pd.Series): Die Zielvariable für die Testdaten.
+        param_grid (dict): Das Hyperparameter-Raster für GridSearch.
+
+    Returns:
+        bestes Modell, beste Hyperparameter, MAE, MSE, RMSE, R², Residuen und Vorhersagen.
+    """
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid,
+                               scoring='neg_mean_squared_error', cv=5, verbose=1)
+    grid_search.fit(X_train_scaled, y_train)
+
+    y_pred_tuned = grid_search.best_estimator_.predict(X_test_scaled)
+    mae = mean_absolute_error(y_test, y_pred_tuned)
+    mse = mean_squared_error(y_test, y_pred_tuned)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred_tuned)
+    residuals = y_test - y_pred_tuned
+    return grid_search.best_estimator_, grid_search.best_params_, mae, mse, rmse, r2, residuals, y_pred_tuned
+
+
+def save_model(model, filename):
+    """
+    Speichert ein Modell in einer Pickle-Datei.
+
+    Args:
+    model (object): Das zu speichernde Objekt.
+    filename (str): Der Name der Datei, in der das Objekt gespeichert wird.
+
+    Returns:
+    None
+    """
+    try:
+        with open(filename, 'wb') as file:
+            pickle.dump(model, file)
+            print(f"Model saved as {filename}")
+    except Exception as e:
+        print(f"Error saving the model: {e}")
+
+
+def save_plot(filename, dpi=300):
+    """
+    Speichert die aktuelle Matplotlib-Grafik als Datei.
+
+    Args:
+    filename (str): Der Name der Datei.
+    dpi (int): Die Auflösung der Grafik. Standardmäßig 300 DPI.
+
+    Returns:
+    None
+    """
+    try:
+        plt.savefig(filename, dpi=dpi, bbox_inches='tight')
+        print(f"Plot saved as {filename}")
+    except Exception as e:
+        print(f"Error saving plot: {e}")
